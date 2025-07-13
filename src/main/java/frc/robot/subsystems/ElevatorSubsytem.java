72eabc1d094bb3642517594b3cc777fd337d1285
muriloMiranda404 <murilomiranda404@gmail.com>
2025-07-13 18:20:05 -0300

    atualização

--- src/main/java/frc/robot/subsystems/ElevatorSubsytem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsytem.java
@@ -23,49 +23,49 @@ public class ElevatorSubsytem extends SubsystemBase{
         return elevator;
     }
 
-    public static  SparkMaxConfig rightMotorConfig;
-    public static  SparkMaxConfig leftMotorConfig;
+    public static SparkMaxConfig rightMotorConfig;
+    public static SparkMaxConfig leftMotorConfig;
     
-    public static  SparkMax rightMotor;
-    public static  SparkMax leftMotor;
+    public static SparkMax rightMotor;
+    public static SparkMax leftMotor;
     
-    public static  PIDController controller;
+    public static PIDController controller;
     
-        public static  DigitalInput upSwitch;
-        public static  DigitalInput downSwitch;
+    public static DigitalInput upSwitch;
+    public static DigitalInput downSwitch;
 
-        public static Encoder encoder;
+    public static Encoder encoder;
 
         
-        private ElevatorSubsytem(){
+    private ElevatorSubsytem(){
             
-            rightMotorConfig = new SparkMaxConfig();
-            leftMotorConfig = new SparkMaxConfig();
+        rightMotorConfig = new SparkMaxConfig();
+        leftMotorConfig = new SparkMaxConfig();
             
-            rightMotorConfig
-            .inverted(false)
-            .idleMode(IdleMode.kCoast);
+        rightMotorConfig
+        .inverted(false)
+        .idleMode(IdleMode.kCoast);
 
-            leftMotorConfig
-            .inverted(true)
-            .idleMode(IdleMode.kCoast);
+        leftMotorConfig
+        .inverted(true)
+        .idleMode(IdleMode.kCoast);
 
-            leftMotor = new SparkMax(Elevator.LEFT_MOTOR, SparkMax.MotorType.kBrushless);
-            rightMotor = new SparkMax(Elevator.RIGHT_MOTOR, SparkMax.MotorType.kBrushless);
+        leftMotor = new SparkMax(Elevator.LEFT_MOTOR, SparkMax.MotorType.kBrushless);
+        rightMotor = new SparkMax(Elevator.RIGHT_MOTOR, SparkMax.MotorType.kBrushless);
 
-            leftMotor.configure(leftMotorConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
-            rightMotor.configure(rightMotorConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+        leftMotor.configure(leftMotorConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+        rightMotor.configure(rightMotorConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
             
-            controller = Elevator.ELEVATOR_PID;
-            controller.setTolerance(Elevator.TOLERANCE_ELEVATOR);
+        controller = Elevator.ELEVATOR_PID;
+        controller.setTolerance(Elevator.TOLERANCE_ELEVATOR);
 
-            upSwitch = new DigitalInput(Elevator.UP_SWITCH);
-            downSwitch = new DigitalInput(Elevator.DOWN_SWITCH);
-            encoder = new Encoder(Elevator.ENCODER_A, Elevator.ENCODER_B);
+        upSwitch = new DigitalInput(Elevator.UP_SWITCH);
+        downSwitch = new DigitalInput(Elevator.DOWN_SWITCH);
+        encoder = new Encoder(Elevator.ENCODER_A, Elevator.ENCODER_B);
 
-            encoder.setDistancePerPulse(360/2048);
-            encoder.setReverseDirection(true);
-            encoder.reset();
+        encoder.setDistancePerPulse(360/2048);
+        encoder.setReverseDirection(true);
+        encoder.reset();
         }
         
 
@@ -96,7 +96,7 @@ public class ElevatorSubsytem extends SubsystemBase{
             rightMotor.set(output);
            }
            
-        public double getSpeed(){
+       public double getSpeed(){
             return encoder.getRate();
        }
 

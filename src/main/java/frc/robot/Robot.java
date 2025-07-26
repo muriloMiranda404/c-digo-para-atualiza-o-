d71a6c399bda78c515d9caa0d252127e5febf3a3
muriloMiranda404 <murilomiranda404@gmail.com>
2025-07-26 15:05:58 -0300

    atualização

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,8 +2,12 @@ package frc.robot;
 
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.AlingToTarget;
+import frc.robot.commands.ResetPigeon;
 import frc.robot.constants.Constants.Elevator;
 import frc.robot.constants.Constants.Intake;
 import frc.robot.subsystems.ElevatorSubsytem;
@@ -24,14 +28,14 @@ public class Robot extends TimedRobot {
   SwerveSubsystem swerve = RobotContainer.getSwerveInstance();
   
   private final RobotContainer m_robotContainer;
-  
+    
   public Robot() {
     m_robotContainer = new RobotContainer();
   }
 
   @Override
   public void robotInit() {
-
+    
     intake.configureIntake();
 
     elevador.configureElevator();

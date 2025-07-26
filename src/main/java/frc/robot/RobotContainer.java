e404e59dac1fd7e773eb5349e69c51125d11fe04
muriloMiranda404 <murilomiranda404@gmail.com>
2025-07-26 12:33:06 -0300

    atualização

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -35,7 +35,7 @@ public class RobotContainer {
   private static final SwerveSubsystem swerve = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
   private static final LimelightConfig limelight = LimelightConfig.getInstance();
 
-  public final CommandXboxController controleXbox = new CommandXboxController(Controller.DRIVE_CONTROLLER);
+  public final CommandXboxController driveController = new CommandXboxController(Controller.DRIVE_CONTROLLER);
   public final XboxController intakeController = new XboxController(Controller.INTAKE_CONTROL_ID);
   
   public static final ElevatorSubsytem elevatorSubsytem = new ElevatorSubsytem();
@@ -49,97 +49,105 @@ public class RobotContainer {
       () -> MathUtil.applyDeadband(setChoose(1), Controller.DEADBAND),
       () -> MathUtil.applyDeadband(setChoose(2), Controller.DEADBAND),
       () -> MathUtil.applyDeadband(setChoose(3), Controller.DEADBAND)));
-    configureBindings();
+      
+    configureDriveBindings();
+    configureMechanismBindings();
   }
 
-  private void configureBindings() {   
-    //posições do elevador CORAL
-    NamedCommands.registerCommand("L4", new ElevatorCommand(elevatorSubsytem, Elevator.L4_POSITION));
-    NamedCommands.registerCommand("L3", new ElevatorCommand(elevatorSubsytem, Elevator.L3_POSITION));
-    NamedCommands.registerCommand("L2", new ElevatorCommand(elevatorSubsytem, Elevator.L2_POSITION));
-    NamedCommands.registerCommand("L1", new ElevatorCommand(elevatorSubsytem, Elevator.L1_POSITION));
+  private void configureDriveBindings() {   
     
-    //posições do elevador ALGA
-    NamedCommands.registerCommand("ALGAE L2", new ElevatorCommand(elevatorSubsytem, Elevator.L2_ALGAE));
-    NamedCommands.registerCommand("ALGAE L3", new ElevatorCommand(elevatorSubsytem, Elevator.L3_ALGAE));
+    //limelight
+    new POVButton(driveController.getHID(), 270).whileTrue(new AlingToTarget(limelight, swerve, true));
 
-    //posições do intake
-    NamedCommands.registerCommand("ALGAE POSITION", new IntakePosition(intakeSubsystem, Intake.ALGAE_POSITION));
-    NamedCommands.registerCommand("ABERTURA L1", new IntakePosition(intakeSubsystem, Intake.ABERTURA_L1));
-    NamedCommands.registerCommand("POSIÇÃO ABERTURA", new IntakePosition(intakeSubsystem, Intake.ABERTURA_COMUM));
-    NamedCommands.registerCommand("CORAL L4", new IntakePosition(intakeSubsystem, Intake.CORAL_L4));
-    NamedCommands.registerCommand("POSIÇÃO MINIMA L1", new IntakePosition(intakeSubsystem, Intake.MIN_INTAKE));
-    NamedCommands.registerCommand("GIRAR CORAL", new IntakeSpeed(intakeSubsystem, 0.8));
-    NamedCommands.registerCommand("GIRAR CORAL INVERTIDO", new IntakeSpeed(intakeSubsystem, -0.8));
-    
-  ///////////////////////////////////////////// COMANDOS TELEOPERADOS////////////////////////////////////////////////////////////
+    //reset pigeon
+    new JoystickButton(driveController.getHID(), 10).onTrue(new ResetPigeon(pigeon, swerve));
 
-    //L1
-    new JoystickButton(intakeController, 1).onTrue(NamedCommands.getCommand("ABERTURA L1")
-    .andThen(NamedCommands.getCommand("L1"))
-    .andThen(NamedCommands.getCommand("POSIÇÃO MINIMA L1"))
-    );
+    //turn robot
+    new JoystickButton(driveController.getHID(), 1).onTrue(new TurnRobot(pigeon, swerve, 45));
+    new JoystickButton(driveController.getHID(), 2).onTrue(new TurnRobot(pigeon, swerve, -45));
+  }
 
-    //L2
-    new JoystickButton(intakeController, 2).onTrue(NamedCommands.getCommand("POSIÇÃO ABERTURA")
-    .andThen(NamedCommands.getCommand("L2"))
-    );
+  public void configureMechanismBindings(){
 
-    //L3
-    new JoystickButton(intakeController, 3).onTrue(NamedCommands.getCommand("POSIÇÃO ABERTURA")
-    .andThen(NamedCommands.getCommand("L3"))
-    );
+     //posições do elevador CORAL
+     NamedCommands.registerCommand("L4", new ElevatorCommand(elevatorSubsytem, Elevator.L4_POSITION));
+     NamedCommands.registerCommand("L3", new ElevatorCommand(elevatorSubsytem, Elevator.L3_POSITION));
+     NamedCommands.registerCommand("L2", new ElevatorCommand(elevatorSubsytem, Elevator.L2_POSITION));
+     NamedCommands.registerCommand("L1", new ElevatorCommand(elevatorSubsytem, Elevator.L1_POSITION));
+     
+     //posições do elevador ALGA
+     NamedCommands.registerCommand("ALGAE L2", new ElevatorCommand(elevatorSubsytem, Elevator.L2_ALGAE));
+     NamedCommands.registerCommand("ALGAE L3", new ElevatorCommand(elevatorSubsytem, Elevator.L3_ALGAE));
+ 
+     //posições do intake
+     NamedCommands.registerCommand("ALGAE POSITION", new IntakePosition(intakeSubsystem, Intake.ALGAE_POSITION));
+     NamedCommands.registerCommand("ABERTURA L1", new IntakePosition(intakeSubsystem, Intake.ABERTURA_L1));
+     NamedCommands.registerCommand("POSIÇÃO ABERTURA", new IntakePosition(intakeSubsystem, Intake.ABERTURA_COMUM));
+     NamedCommands.registerCommand("CORAL L4", new IntakePosition(intakeSubsystem, Intake.CORAL_L4));
+     NamedCommands.registerCommand("POSIÇÃO MINIMA L1", new IntakePosition(intakeSubsystem, Intake.MIN_INTAKE));
+     NamedCommands.registerCommand("GIRAR CORAL", new IntakeSpeed(intakeSubsystem, 0.8));
+     NamedCommands.registerCommand("GIRAR CORAL INVERTIDO", new IntakeSpeed(intakeSubsystem, -0.8));
+     
+   ///////////////////////////////////////////// COMANDOS TELEOPERADOS////////////////////////////////////////////////////////////
+ 
+     //L1
+     new JoystickButton(intakeController, 1).onTrue(NamedCommands.getCommand("ABERTURA L1")
+     .andThen(NamedCommands.getCommand("L1"))
+     .andThen(NamedCommands.getCommand("POSIÇÃO MINIMA L1"))
+     );
+ 
+     //L2
+     new JoystickButton(intakeController, 2).onTrue(NamedCommands.getCommand("POSIÇÃO ABERTURA")
+     .andThen(NamedCommands.getCommand("L2"))
+     );
+ 
+     //L3
+     new JoystickButton(intakeController, 3).onTrue(NamedCommands.getCommand("POSIÇÃO ABERTURA")
+     .andThen(NamedCommands.getCommand("L3"))
+     );
+ 
+     //L4
+     new JoystickButton(intakeController, 4).onTrue(NamedCommands.getCommand("POSIÇÃO ABERTURA")
+     .andThen(NamedCommands.getCommand("L4"))
+     .andThen(NamedCommands.getCommand("CORAL L4"))
+     );
 
-    //L4
-    new JoystickButton(intakeController, 4).onTrue(NamedCommands.getCommand("POSIÇÃO ABERTURA")
-    .andThen(NamedCommands.getCommand("L4"))
-    .andThen(NamedCommands.getCommand("CORAL L4"))
-    );
-    
-    //empurrar e puxar o coral
+         //empurrar e puxar o coral
     new JoystickButton(intakeController, 5).whileTrue(NamedCommands.getCommand("GIRAR CORAL"));
     new JoystickButton(intakeController, 6).whileTrue(NamedCommands.getCommand("GIRAR CORAL INVERTIDO"));
 
-    //limelight
-    new POVButton(controleXbox.getHID(), 0).onTrue(new AlingToTarget(limelight, swerve, true));
-    new POVButton(controleXbox.getHID(), 270).whileTrue(new AlingToTarget(limelight, swerve, 25.0, 14.0));
 
-    //reset pigeon && turn robot
-    new JoystickButton(controleXbox.getHID(), 10).onTrue(new ResetPigeon(pigeon, swerve));
-    new JoystickButton(controleXbox.getHID(), 2).onTrue(new TurnRobot(pigeon, swerve, 45));
-
-    ////////////////////////////////////// FIM DO COMANDO TELEOPERADO////////////////////////////////////////////////////
-
-    ////////////////////////////////////// COMANDOS AUTOMATICOS////////////////////////////////////////////////////////
+      //////////////////////////////////////INICIO DOS COMANDOS AUTOMATICOS////////////////////////////////////////////////////////
 
     //L1
     NamedCommands.registerCommand("L1 FULL COMMAND", 
-      NamedCommands.getCommand("ABERTURA L1")
-      .andThen(NamedCommands.getCommand("L1"))
-      .andThen(NamedCommands.getCommand("POSIÇÃO MINIMA L1"))
-      );
-    //L2
-
-    NamedCommands.registerCommand("L2 FULL COMMAND", 
-    NamedCommands.getCommand("POSIÇÃO ABERTURA")
-    .andThen(NamedCommands.getCommand("L2"))
-    );  
-
-    //L3
-    NamedCommands.registerCommand("L3 FULL COMMAND", 
-    NamedCommands.getCommand("POSIÇÃO ABERTURA")
-    .andThen(NamedCommands.getCommand("L3"))
+    NamedCommands.getCommand("ABERTURA L1")
+    .andThen(NamedCommands.getCommand("L1"))
+    .andThen(NamedCommands.getCommand("POSIÇÃO MINIMA L1"))
     );
+  //L2
 
-    //L4  
-    NamedCommands.registerCommand("L4 FULL COMMAND", 
-    NamedCommands.getCommand("POSIÇÃO ABERTURA")
-    .andThen(NamedCommands.getCommand("L4"))
-    .andThen(NamedCommands.getCommand("CORAL L4"))
-    );
+  NamedCommands.registerCommand("L2 FULL COMMAND", 
+  NamedCommands.getCommand("POSIÇÃO ABERTURA")
+  .andThen(NamedCommands.getCommand("L2"))
+  );  
+
+  //L3
+  NamedCommands.registerCommand("L3 FULL COMMAND", 
+  NamedCommands.getCommand("POSIÇÃO ABERTURA")
+  .andThen(NamedCommands.getCommand("L3"))
+  );
+
+  //L4  
+  NamedCommands.registerCommand("L4 FULL COMMAND", 
+  NamedCommands.getCommand("POSIÇÃO ABERTURA")
+  .andThen(NamedCommands.getCommand("L4"))
+  .andThen(NamedCommands.getCommand("CORAL L4"))
+  );
 
 
-    ///////////////////////////////////////// FIM DOS COMANDOS AUTOMATICOS ///////////////////////////////////////////////
+  ///////////////////////////////////////// FIM DOS COMANDOS AUTOMATICOS ///////////////////////////////////////////////
+    
   }
   public Command getAutonomousCommand() {
     return new PathPlannerAuto(Autonomous.AUTO);
@@ -151,17 +159,17 @@ public class RobotContainer {
     int inverter = DriverStation.getAlliance().get() == Alliance.Red ? -1 : 1;
     double marcha = 0.5;
     
-    if(controleXbox.getHID().getRightBumperButton()) marcha = 1.0;
-    if(controleXbox.getHID().getLeftBumperButton()) marcha = 0.2;
+    if(driveController.getHID().getRightBumperButton()) marcha = 1.0;
+    if(driveController.getHID().getLeftBumperButton()) marcha = 0.2;
     else marcha = 0.5;
  
     switch (choose) {
       case 1:
-        return controleXbox.getLeftY() * inverter * marcha;
+        return driveController.getLeftY() * inverter * marcha;
       case 2:
-      return controleXbox.getLeftX() * inverter * marcha;
+      return driveController.getLeftX() * inverter * marcha;
       case 3:
-      return controleXbox.getRightX() * marcha;  
+      return driveController.getRightX() * marcha;  
     }
     
     return choose;
